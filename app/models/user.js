77dace260c2c98bc353c9c1dd91e7b310d3117c1
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2016-10-17 15:48:01 -0400

    Added a getUsername method for user model

--- app/models/user.js
+++ app/models/user.js
@@ -33,14 +33,22 @@ var userSchema = mongoose.Schema({
 
 // methods ======================
 // generating a hash
-userSchema.methods.generateHash = function(password) {
+userSchema.methods.generateHash = function (password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
-userSchema.methods.validPassword = function(password) {
+userSchema.methods.validPassword = function (password) {
     return bcrypt.compareSync(password, this.local.password);
 };
+
+userSchema.methods.getUsername = function () {
+    if (this.local.name) return this.local.name;
+    else if (this.facebook.name) return this.facebook.name;
+    else if (this.twitter.name) return this.twitter.name;
+    else if (this.google.name) return this.google.name;
+    else return "";
+}
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
