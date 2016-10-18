4f15181ab1fcbe83e1fd94ff572676efefb86c18
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2016-10-17 22:46:50 -0400

    fixed twitter display name reference

--- app/models/user.js
+++ app/models/user.js
@@ -9,6 +9,7 @@ var userSchema = mongoose.Schema({
     local            : {
         email        : String,
         password     : String,
+        name         : String
     },
     facebook         : {
         id           : String,
@@ -45,7 +46,7 @@ userSchema.methods.validPassword = function (password) {
 userSchema.methods.getUsername = function () {
     if (this.local.name) return this.local.name;
     else if (this.facebook.name) return this.facebook.name;
-    else if (this.twitter.name) return this.twitter.name;
+    else if (this.twitter.displayName) return this.twitter.displayName;
     else if (this.google.name) return this.google.name;
     else return "";
 }
